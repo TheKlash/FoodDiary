a7d1c2ad07b6c780a8ecefe1619c69046e8d072f
kexnoon <alexeypopov566@gmail.com>
2024-02-20 02:38:46 +0100

    version 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.alekseipopov.fooddiary"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
